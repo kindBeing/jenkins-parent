866a102b0f5a6be1384474bcf43ed61c2bf87bf3
Nikhil <kindbeing@github.com>
2022-02-16 23:59:50 -0500

    Next try

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,24 +1,22 @@
 package jenkins.parent.vars
 
-def call () {
-    pipeline {
-        stages {
-            stage('Build') {
-                steps {
-                    echo 'Building..'
-                    println("Building.. ${config.AppName}")
-                    println("Building.. ${config.Version}")
-                }
+def call() {
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+                println("Building.. ${config.AppName}")
+                println("Building.. ${config.Version}")
             }
-            stage('Test') {
-                steps {
-                    echo 'Testing..'
-                }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
             }
-            stage('Deploy') {
-                steps {
-                    echo 'Deploying....'
-                }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
             }
         }
     }
